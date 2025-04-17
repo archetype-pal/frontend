91e8db4ff92062400cb2e4697b5faf7a0adcf509
Phaedon Koutsoyannopoulos <Faidon.Koutsogiannopoulos@glasgow.ac.uk>
2025-04-17 03:33:15 +0100

    feat: Search - moved result types to be a bar at the top of the searc… (#12)
    * feat: Search - moved result types to be a bar at the top of the search page
    
    * feat: switch result type toggle to single-select mode
    
    * fix: removes committed personal configuration
    
    ---------
    
    Co-authored-by: Ahmed Elghareeb <ahmed.elghareeb@proton.me>

--- components/manuscripts-search.tsx
+++ components/manuscripts-search.tsx
@@ -3,12 +3,14 @@
 import * as React from 'react'
 import { ManuscriptsFilters } from './manuscripts-filters'
 import { ManuscriptsTable } from './manuscripts-table'
+import { ResultTypeToggle } from './search-result-types'
 import { Button } from '@/components/ui/button'
 import { Grid, List } from 'lucide-react'
 
 export function ManuscriptsSearch() {
   const [, setViewMode] = React.useState<'table' | 'grid'>('table')
-
+  const [resultType, setResultType] = React.useState<string>('')
+  
   return (
     <div className='h-screen bg-gray-50'>
       <div className='px-6 py-4 border-b bg-white'>
@@ -34,8 +36,18 @@ export function ManuscriptsSearch() {
       </div>
       <div className='flex h-[calc(100vh-73px)]'>
         <ManuscriptsFilters />
-        <div className='flex-1 p-6 overflow-auto'>
-          <ManuscriptsTable />
+        <div className='flex flex-1 flex-col'>
+          <div className='px-6 h-[150px] border-b bg-white grid content-center'>
+
+            <ResultTypeToggle
+              selectedType={resultType}
+              onChange={setResultType}
+            />
+
+          </div>
+          <div className='p-6 overflow-auto'>
+            <ManuscriptsTable />
+          </div>
         </div>
       </div>
     </div>
